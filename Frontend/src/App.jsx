1528fc7fce072aa1fef96e4c35989571876ebad9
Pratyay Mahato <pratyaymahato44@gmail.com>
2026-09-02 20:01:36 +0530

    Add Footer, Layout,Navber,pageLoader,SentryErrorFallback and SentryuserSync

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/react'
+import PageLoader from './components/PageLoader'
+import Layout from './components/Layout'
 
 function App() {
+  const {isLoaded} = useAuth()
 
+  if(!isLoaded) return <PageLoader />
 
   return (
-    <>
-    <h1>This is a Ecommerse Website</h1>
+    
+    <Layout>
+    <h1 className='text-3xl text-center'>This is a Ecommerse Website</h1>
      <header>
         <Show when="signed-out">
           <SignInButton mode='modal'/>
@@ -15,7 +20,10 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
+
+      <button className='btn btn-primary'>click me</button>
+      </Layout>
+    
     
   )
 }
